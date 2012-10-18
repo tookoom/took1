b17bde51840196d3a7be68c4b7dde224654a8c0b
andre.v.mattos <andre.v.mattos@03a878d5-8350-9059-cb28-bc5b534a98d1>
2012-10-18 12:50:26 +0000

    RIA Servces Versão 1

---
+++ Code/TK1.Silverlight.Data/Collection/BrokerSiteAdCollections.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using TK1.Bizz.Data;
+using System.Collections.ObjectModel;
+
+namespace TK1.Silverlight.Data.Collection
+{
+    public class SiteAdDetailCollection : ObservableCollection<SiteAdDetail>
+    {
+
+    }
+    public class SiteAdPicCollection : ObservableCollection<SiteAdPic>
+    {
+
+    }
+    public class SiteAdStatusCollection : ObservableCollection<SiteAdStatu>
+    {
+
+    }
+}
---
+++ Code/TK1.Web/Extension/PageExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace TK1.Web.Extension
+{
+    public static class PageExtension
+    {
+        public static int GetSessionIntegerValue(this Page page, string key)
+        {
+            int result = -1;
+            var value = page.Session[key] == null ? string.Empty : page.Session[key].ToString();
+            if (!string.IsNullOrEmpty(value))
+            {
+                int.TryParse(value, out result);
+            }
+            return result; 
+        }
+        public static string GetSessionStringValue(this Page page, string key)
+        {
+            return page.Session[key] == null ? null : page.Session[key].ToString();
+        }
+        public static object GetSessionValue(this Page page, string key)
+        {
+            return page.Session[key];
+        }
+        public static void SetSessionValue(this Page page, string key, object value)
+        {
+            page.Session[key] = value;
+  
+        }
+    }
+}
