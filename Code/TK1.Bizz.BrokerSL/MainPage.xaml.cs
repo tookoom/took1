f651d36d06cd924bbcba0b59f7e7631c4fc949ef
andre.v.mattos <andre.v.mattos@03a878d5-8350-9059-cb28-bc5b534a98d1>
2012-10-18 12:46:37 +0000

    Broker SL versão 1

---
+++ Code/TK1.Bizz.BrokerSL/MainPage.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using TK1.Silverlight.Data;
+using TK1.Bizz.Data;
+using System.ServiceModel.DomainServices.Client;
+using TK1.Bizz.Data.Presentation;
+using TK1.Silverlight.Data.Web;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TK1.Bizz.BrokerSL.BrokerFileServiceReference;
+
+namespace TK1.Bizz.BrokerSL
+{
+	public partial class MainPage : UserControl
+	{
+        #region PRIVATE MEMBERS
+        private BrokerContext brokerContext;
+        private IEnumerable<SiteAd> siteAdsEntities;
+        //private IEnumerable<SiteAdIDGenerator> siteAdIDGeneratorsEntities;
+        private IEnumerable<SiteAdStatu> siteAdStatusEntities;
+
+
+        #endregion
+
+		public MainPage()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+
+            brokerContext = new BrokerContext();
+
+            siteAdsEntities = brokerContext.Load(brokerContext.GetCustomerSiteAdsQuery("pandolfo")).Entities;
+            siteAdStatusEntities = brokerContext.Load(brokerContext.GetSiteAdStatusQuery()).Entities;
+            brokerContext.Load(brokerContext.GetSiteAdTypesQuery());
+
+            setDefaultState();
+            searchSiteAds();
+
+        }
+
+        private void addSiteAd()
+        {
+            var adType = SiteAdTypes.Rent;
+            if (radioButtonSell.IsChecked.HasValue & radioButtonSell.IsChecked.Value)
+                adType = SiteAdTypes.Sell;
+
+            var siteAd = new SiteAd()
+            {
+                CategoryName = "Residencial/Comercial",
+                CustomerCodename = "pandolfo",
+                CityName = "Porto Alegre",
+                DistrictName = "Bairro",
+                FeaturedAd = false,
+                SiteAdStatusID = 1,
+                SiteAdTypeID = (int)adType,
+                SiteTypeName = "Apartamento/Casa/Loja",
+                Visible = false
+            };
+
+            brokerContext.GenerateSiteAdID("pandolfo", adType, dataContextGenerateSiteAdID_Completed, siteAd);
+        }
+        private void setCodeSearchControlsState()
+        {
+            labelSiteAdCodeSearch.IsEnabled = checkBoxSiteAdCodeSearch.IsChecked.Value;
+            textBoxSiteCode.IsEnabled = checkBoxSiteAdCodeSearch.IsChecked.Value;
+
+        } 
+        private void setDefaultState()
+        {
+            setCodeSearchControlsState();
+        }
+        private void searchSiteAds()
+        {
+            var customerCodename = "pandolfo";
+            var adType = SiteAdTypes.Rent;
+            if (radioButtonSell.IsChecked.HasValue & radioButtonSell.IsChecked.Value)
+                adType = SiteAdTypes.Sell;
+
+            LoadOperation<SiteAd> resultSet = brokerContext.Load(brokerContext.GetSiteAdsQuery(customerCodename, adType), dataContextSiteAd_OnLoadCompleted, null);
+        }
+        //private static void testUpload()
+        //{
+        //    var fileDialog = new OpenFileDialog();
+        //    fileDialog.Filter = "Fotos|*.jpg";
+        //    bool? result = fileDialog.ShowDialog();
+        //    if (result != null && result == true)
+        //    {
+        //        Stream stream = fileDialog.File.OpenRead();
+        //        byte[] buffer = new byte[stream.Length]; stream.Read(buffer, 0, (int)stream.Length);
+        //        stream.Dispose();
+        //        stream.Close();
+
+        //        UploadPicture uploadPicture = new UploadPicture();
+        //        uploadPicture.FileName = fileDialog.File.Name;
+        //        uploadPicture.File = buffer;
+        //        uploadPicture.Thumbnail = buffer;
+
+        //        BrokerFileServiceClient serviceClient = new BrokerFileServiceClient();
+        //        serviceClient.SaveBrokerSiteAdPicAsync("pandolfo", Data.Presentation.SiteAdTypes.Rent, 1, uploadPicture);
+        //    }
+        //}
+
+        #region DATA CONTEXT EVENT HANDLERS
+        private void dataContextGenerateSiteAdID_Completed(InvokeOperation<int> invokeOperation)
+        {
+            if (invokeOperation.HasError)
+            {
+                MessageBox.Show(string.Format("Falha na geração de código para imóvel: {0}", invokeOperation.Error));
+                invokeOperation.MarkErrorAsHandled();
+            }
+            else
+            {
+                var siteAd = invokeOperation.UserState as SiteAd;
+                if (siteAd != null)
+                {
+                    siteAd.SiteAdID = invokeOperation.Value;
+                    brokerContext.SiteAds.Add(siteAd);
+                    brokerContext.SubmitChanges();
+                    searchSiteAds();
+                }
+            }
+        }
+        private void dataContextSiteAd_OnLoadCompleted(LoadOperation<SiteAd> loadOperation)
+        {
+            if (loadOperation.HasError)
+            {
+                MessageBox.Show(string.Format("Falha na pesquisa: {0}", loadOperation.Error));
+                loadOperation.MarkErrorAsHandled();
+            }
+            else
+            {
+                dataGridSiteAdsResult.ItemsSource = loadOperation.Entities;
+            }
+        } 
+        #endregion
+
+        #region UI EVENT HANDLERS
+        private void buttonSearchSite_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            searchSiteAds();
+
+        }
+        private void buttonSiteAdCreate_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            addSiteAd();
+
+        }
+
+        private void buttonSiteAdEdit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var siteAd = dataGridSiteAdsResult.SelectedItem as SiteAd;
+            if (siteAd != null)
+            {
+                SiteAdEditor siteAdEditor = new SiteAdEditor();
+                siteAdEditor.BrokerContext = brokerContext;
+                siteAdEditor.DataContext = siteAd;
+                siteAdEditor.SiteAdsEntities = siteAdsEntities;
+                siteAdEditor.SiteAdStatusEntities = siteAdStatusEntities;
+                siteAdEditor.Show();
+            }
+        }
+        private void buttonSiteAdRemove_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var siteAd = dataGridSiteAdsResult.SelectedItem as SiteAd;
+            if (siteAd != null)
+            {
+                var result = MessageBox.Show("Deseja remover este imóvel? Este operação não poderá ser desfeita", "Remover Imóvel", MessageBoxButton.OKCancel);
+                if (result == MessageBoxResult.OK)
+                {
+                    brokerContext.SiteAds.Remove(siteAd);
+                    brokerContext.SubmitChanges();
+                    searchSiteAds();
+
+                }
+            }
+        }
+
+        private void buttonSave_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if(brokerContext.HasChanges)
+                brokerContext.SubmitChanges();
+        }
+        private void buttonCancel_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var client = new BrokerFileServiceClient();
+            client.DoWorkAsync();
+
+            brokerContext.RejectChanges();
+        }
+
+        private void checkBoxSiteAdCodeSearch_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            setCodeSearchControlsState();
+        }
+
+        private void radioButtonRent_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            searchSiteAds();
+        }
+        private void radioButtonSell_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            searchSiteAds();
+        }
+
+        #endregion
+
+
+	}
+}
---
+++ Code/TK1.Bizz.BrokerSL/SiteAdEditor.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using TK1.Silverlight.Data.Web;
+using System.ServiceModel.DomainServices.Client;
+using TK1.Silverlight.Data.Collection;
+using System.IO;
+using TK1.Bizz.BrokerSL.BrokerFileServiceReference;
+using TK1.Bizz.Data;
+
+namespace TK1.Bizz.BrokerSL
+{
+    public partial class SiteAdEditor : ChildWindow
+    {
+        #region PRIVATE MEMBERS
+        private BrokerContext brokerContext;
+
+        #endregion        
+
+        #region PUBLIC PROPERTIES
+        public BrokerContext BrokerContext
+        {
+            get { return brokerContext; }
+            set { brokerContext = value; }
+        }
+        public object DataContext
+        {
+            get { return base.DataContext; }
+            set
+            {
+                base.DataContext = value;
+                loadSiteAd(value as SiteAd);
+            }
+        }
+        public IEnumerable<SiteAd> SiteAdsEntities
+        {
+            set
+            {
+                if(autoCompleteBoxCityNames!= null)
+                    autoCompleteBoxCityNames.ItemsSource = value.Select(o => o.CityName).Distinct().OrderBy(o => o);
+                if (autoCompleteBoxDistrictNames != null)
+                    autoCompleteBoxDistrictNames.ItemsSource = value.Select(o => o.DistrictName).Distinct().OrderBy(o => o);
+                if (autoCompleteBoxCategoryNames != null)
+                    autoCompleteBoxCategoryNames.ItemsSource = value.Select(o => o.CategoryName).Distinct().OrderBy(o => o);
+                if (autoCompleteBoxSiteTypeNames != null)
+                    autoCompleteBoxSiteTypeNames.ItemsSource = value.Select(o => o.SiteTypeName).Distinct().OrderBy(o => o);
+            }
+        }
+        public IEnumerable<SiteAdStatu> SiteAdStatusEntities
+        {
+            set
+            {
+                if (comboBoxStatus != null)
+                    comboBoxStatus.ItemsSource = value;
+            }
+        } 
+        #endregion
+
+        public SiteAdEditor()
+        {
+            InitializeComponent();
+
+            loadConstraints();
+        }
+
+        private void addSiteAdPic()
+        {
+            var siteAd = DataContext as SiteAd;
+            if (siteAd != null)
+            {
+
+                BrokerFileServiceClient serviceClient = new BrokerFileServiceClient();
+                var fileDialog = new OpenFileDialog();
+                fileDialog.Filter = "Arquivos de Fotos|*.jpg";
+                bool? result = fileDialog.ShowDialog();
+                if (result != null && result == true)
+                {
+                    int picID = 0;
+                    if (siteAd.SiteAdPics != null & siteAd.SiteAdPics.Count > 0)
+                        picID = siteAd.SiteAdPics.Max(o => o.PicID);
+                    picID += 1;
+                    var siteAdPic = new SiteAdPic()
+                    {
+                        FileName = fileDialog.File.Name,
+                        PicID = picID
+                    };
+                    siteAd.SiteAdPics.Add(siteAdPic);
+                    dataGridSiteAdPics.ItemsSource = siteAd.SiteAdPics;
+
+                    Stream stream = fileDialog.File.OpenRead();
+                    byte[] buffer = new byte[stream.Length]; stream.Read(buffer, 0, (int)stream.Length);
+                    stream.Dispose();
+                    stream.Close();
+
+                    UploadPicture file = new UploadPicture();
+                    file.FileName = fileDialog.File.Name;
+                    file.File = buffer;
+                    file.Thumbnail = buffer;
+                    serviceClient.SaveBrokerSiteAdPicAsync(siteAd.CustomerCodename, (Data.Presentation.SiteAdTypes)siteAd.SiteAdTypeID, siteAd.SiteAdID, file, siteAdPic);
+                    serviceClient.SaveBrokerSiteAdPicCompleted += new EventHandler<SaveBrokerSiteAdPicCompletedEventArgs>(service_SaveFileCompleted);
+                }
+            }
+        }
+        private void loadConstraints()
+        {
+            //BrokerContext brokerContext = new BrokerContext();
+            //autoCompleteBoxCityNames.ItemsSource = brokerContext.Load(brokerContext.GetCustomerSiteAdsQuery("pandolfo")).Entities.Select(o => o.CityName).Distinct().OrderBy(o => o);
+            //autoCompleteBoxCategoryNames.ItemsSource = brokerContext.Load(brokerContext.GetCustomerSiteAdsQuery("pandolfo")).Entities.Select(o => o.CategoryName).Distinct().OrderBy(o => o);
+        }
+        private void loadSiteAd(SiteAd siteAd)
+        {
+            if (siteAd != null)
+            {
+                brokerContext.Load(brokerContext.GetSiteAdDetailsQuery("pandolfo", (Data.Presentation.SiteAdTypes)siteAd.SiteAdTypeID, siteAd.SiteAdID), dataContextSiteAdDetails_OnLoadCompleted, null);
+                brokerContext.Load(brokerContext.GetSiteAdPicsQuery("pandolfo", (Data.Presentation.SiteAdTypes)siteAd.SiteAdTypeID, siteAd.SiteAdID), dataContextSiteAdPics_OnLoadCompleted, null);
+                //dataFormSiteAdDetails.ItemsSource = brokerContext.Load(brokerContext.GetSiteAdDetailsQuery("pandolfo", (Data.Presentation.SiteAdTypes)siteAd.SiteAdTypeID,siteAd.SiteAdID)).Entities;
+                //dataFormSiteAdDetails.ItemsSource = brokerContext.Load(brokerContext.GetSiteAdPicsQuery("pandolfo", (Data.Presentation.SiteAdTypes)siteAd.SiteAdTypeID, siteAd.SiteAdID)).Entities.OrderBy(o=>o.PicID);
+            }
+        }
+
+        #region UI EVENT HANDLERS
+        private void buttonAddSiteAdPic_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            addSiteAdPic();
+        }
+        private void buttonSiteAdPicRemove_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var siteAd = DataContext as SiteAd;
+            if (siteAd != null)
+            {
+                var siteAdPic = dataGridSiteAdPics.SelectedItem as SiteAdPic;
+                if (siteAdPic != null)
+                {
+                    siteAd.SiteAdPics.Remove(siteAdPic);
+                    dataGridSiteAdPics.ItemsSource = siteAd.SiteAdPics;
+                }
+            }
+        }
+        private void buttonGetFilePath_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        }
+
+        
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            if (brokerContext != null)
+                brokerContext.SubmitChanges();
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            if (brokerContext != null)
+                brokerContext.RejectChanges();
+        }
+        
+        #endregion
+
+        #region DATA CONTEXT EVENT HANDLERS
+        private void dataContextSiteAdDetails_OnLoadCompleted(LoadOperation<SiteAdDetail> loadOperation)
+        {
+            if (loadOperation.HasError)
+            {
+                MessageBox.Show(string.Format("Retrieving data failed: {0}", loadOperation.Error));
+                loadOperation.MarkErrorAsHandled();
+            }
+            else
+            {
+                // dataFormSiteAdDetails.ItemsSource = loadOperation.Entities as SiteAdDetailCollection;
+                //dataFormSiteAdDetails.DataContext = loadOperation.Entities;
+            }
+        }
+        private void dataContextSiteAdPics_OnLoadCompleted(LoadOperation<SiteAdPic> loadOperation)
+        {
+            if (loadOperation.HasError)
+            {
+                MessageBox.Show(string.Format("Retrieving data failed: {0}", loadOperation.Error));
+                loadOperation.MarkErrorAsHandled();
+            }
+            else
+            {
+                dataGridSiteAdPics.ItemsSource = loadOperation.Entities;
+            }
+        }
+        private void service_SaveFileCompleted(object sender, SaveBrokerSiteAdPicCompletedEventArgs e)
+        {
+            bool fileSaveSuccess = false;
+            if (e.Error == null & e.Result != null)
+            {
+                var siteAdPic = e.UserState as SiteAdPic;
+                if (siteAdPic != null)
+                {
+                    if (e.Result.Success)
+                    {
+                        siteAdPic.PictureUrl = e.Result.PictureUrl;
+                        siteAdPic.PictureFilePath = e.Result.PicturePath;
+                        siteAdPic.ThumbnailFilePath = e.Result.ThumbnaiPath;
+                        siteAdPic.ThumbnailUrl = e.Result.ThumbnailUrl;
+                        fileSaveSuccess = true;
+                    }
+                }
+            }
+            if (fileSaveSuccess)
+            {
+                MessageBox.Show("Arquivo salvo com sucesso");
+            }
+            else
+            {
+                MessageBox.Show("Falha no envio do arquivo");
+            }
+        }
+
+        #endregion
+
+    }
+}
+
