451741c6ea9f564e291c4a080cfc6a02b6cfb3a1
andre.v.mattos <andre.v.mattos@03a878d5-8350-9059-cb28-bc5b534a98d1>
2013-05-07 03:20:23 +0000

    Versão Inicial

---
+++ trunk/Code/Web.TK1MVC/Web.TK1.Mvc/Areas/Broker/Controllers/BrokerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.TK1.Mvc.Areas.Broker.Controllers
+{
+    public class BrokerController : Controller
+    {
+        //
+        // GET: /Broker/Broker/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ trunk/Code/Web.TK1MVC/Web.TK1.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.TK1.Mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.FeaturedText = "Rápido e fácil.";
+            ViewBag.Message = "Do jeito que você precisa.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Broker.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
