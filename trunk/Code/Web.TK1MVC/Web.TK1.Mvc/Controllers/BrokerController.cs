6f3b3a4e8c44ac2cc0943efed5fffebca50673a3
andre.v.mattos <andre.v.mattos@03a878d5-8350-9059-cb28-bc5b534a98d1>
2013-05-10 01:57:03 +0000

    Alterações bibliotecas Bizz e Client

---
+++ trunk/Code/Web.TK1MVC/Web.TK1.Mvc/Controllers/BrokerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TK1.Bizz.Broker.Presentation;
+using TK1.Data.Bizz.Broker.Controller;
+
+namespace Web.TK1.Mvc.Controllers
+{
+    public class BrokerController : Controller
+    {
+        //
+        // GET: /Broker/
+
+        public ActionResult Index(string id)
+        {
+            var adController = new PropertyAdController(id);
+            var featureAds = adController.GetFeaturedPropertyAds(PropertyAdTypes.Sell, 5);
+            return View(featureAds);
+        }
+
+        public ActionResult Imovel(string id, PropertyAdTypes adType, int adCode)
+        {
+            var adController = new PropertyAdController(id);
+            var ad = adController.GetPropertyAdView(adType, adCode);
+            return View(ad);
+        }
+    }
+}
